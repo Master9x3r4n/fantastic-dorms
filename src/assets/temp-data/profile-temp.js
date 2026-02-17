9c0477f6b60fc4c0cc95478568db5d84491423fc
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-17 23:35:18 +0800

    feat: created profile temp js data

---
+++ src/assets/temp-data/profile-temp.js
@@ -0,0 +1,98 @@
+export const profileData = {
+    "casey_c": {
+	    "name": "Casey Cruz",
+		"username": "casey_c",
+		"joinDate": "March 1st, 2020",
+		"bio": "Archi student. Loves natural light and good coffee.",
+        "listingAdmin": "",
+        "profileImgSrc": "https://i.pinimg.com/736x/07/ad/21/07ad218a0a5df57b1cf0458c7febd2e3.jpg",
+        "schoolData": {
+            "name": "University of Santo Tomas",
+            "since": "2023"
+        },
+        "dormData": {
+            "name": "Metro Haven Suites",
+            "since": "2023"
+        },
+        "reviewData": {
+            "since": "Jan 2023",
+            "reviews": [
+            {
+                "listing": {
+                    "id": 1,
+                    "thumbnailSrc": "https://youre.outof.games/media/uploads/cb/da/cbda1bb4-ee0d-4c65-989f-05a24edd22cf/daily-bugle-featured-location.jpg"
+                },
+                "content": {
+                    "title": "Grand Apartment",
+                    "description": "One of my favorite places ever"
+                },
+                "rating": 4.5
+            },
+            {
+                "listing": {
+                    "id": 2,
+                    "thumbnailSrc": "https://static0.cbrimages.com/wordpress/wp-content/uploads/2020/09/rsz-baxter-buildingv1.jpg"
+                },
+                "content": {
+                    "title": "The Baxter Suite",
+                    "description": "Very Fantastic for us to live in."
+                },
+                "rating": 4
+            },
+            {
+                "listing": {
+                    "id": 3,
+                    "thumbnailSrc": "https://static.wikitide.net/peppafanonwiki/thumb/e/ee/Peppa%27s_house_updated.webp/800px-Peppa%27s_house_updated.webp.png"
+                },
+                "content": {
+                    "title": "Miro\'s House",
+                    "description": "meh"
+                },
+                "rating": 1
+            },           
+            ]
+        },
+    },
+
+    "2": {
+	    "name": "",
+		"username": "",
+		"joinDate": "",
+		"bio": "",
+        "listingAdmin": 0,
+        "profileImgSrc": "",
+        "schoolData": {
+            "name": "",
+            "since": 0
+        },
+        "dormData": {
+            "name": "",
+            "since": ""
+        },
+        "reviewData": {
+            "since": "",
+            "reviews": ["profileReview array"]
+        }
+    },
+
+    "3": {
+	    "name": "",
+		"username": "",
+		"joinDate": "",
+		"bio": "",
+        "listingAdmin": 0,
+        "profileImgSrc": "",
+        "schoolData": {
+            "name": "",
+            "since": 0
+        },
+        "dormData": {
+            "name": "",
+            "since": ""
+        },
+        "reviewData": {
+            "since": "",
+            "reviews": ["profileReview array"]
+        }
+    },
+}
