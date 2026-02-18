fed3a41239b99dc416576d3a9a95fe5c1a1e8db5
Stephen <stephen.jszs.gplay@gmail.com>
2026-02-18 23:32:56 +0800

    feat: adding listing temp data

---
+++ src/assets/temp-data/listing-temp.js
@@ -0,0 +1,125 @@
+export const listingData = {
+    "1": {
+        "name": "Grand Apartment",
+        "description": "Blk 12 Lt 32 Somebody st., Las Pinas City",
+        "address": "A very grand, beautiful, and luxurious apartment that features multiple rooms and doors. A must stay for the holidays.",
+        "owner": "The Daily Bugle",
+        "amenities": ['2 Bedrooms', '1 Bathroom', 'Air Conditioning'],
+        "contacts": ['The Daily Bugle', 'Facebook', 'Twitter'],
+        "rating": [
+            { "name": "Cleanliness", "rating": 4 },
+            { "name": "Comfort", "rating": 4 },
+            { "name": "Spaciousness", "rating": 3 },
+            { "name": "Price", "rating": 2 }
+        ],
+        "mediaSrcs": [
+            "https://youre.outof.games/media/uploads/cb/da/cbda1bb4-ee0d-4c65-989f-05a24edd22cf/daily-bugle-featured-location.jpg",
+
+        ],
+        "locationSrc": "https://cdn.britannica.com/06/276306-049-A8451874/world-map.jpg",
+        "reviews": [
+            {
+                "reviewerProfile": {
+                    "name": "Casey Cruz",
+                    "username": "casey_c",
+                    "profileImgSrc": "https://i.pinimg.com/736x/07/ad/21/07ad218a0a5df57b1cf0458c7febd2e3.jpg",
+                    "count": 2
+                },
+                "content": {
+                    "title": "Le Grand Apartment",
+                    "description": "One of my favorite places ever. I am glad that i decided to visit this place.",
+                    "reply": "Glad to hear that you were satisfied with our space! ^_^"
+                },
+                "rating": 4.5,
+                "score": 67,
+                "mediaSrcs": []
+            },
+            {
+                "reviewerProfile": {
+                    "name": "Jordan Lee",
+                    "username": "jlee_99",
+                    "profileImageSrc": "https://i.pinimg.com/1200x/40/71/e3/4071e33ef6d3d34178fac2a3e11e358b.jpg",
+                    "count": 2
+                },
+                "content": {
+                    "title": "My review",
+                    "description": "Absolutely Grand! That is all I have to say for this wonderful place.",
+                    "reply": ""
+                },
+                "rating": 5,
+                "score": 52,
+                "mediaSrcs": []
+            },
+        ]
+    },
+    
+    "2": {
+        "name": "",
+        "description": "",
+        "address": "",
+        "owner": "",
+        "amenities": ["", ""],
+        "contacts": ["", ""],
+        "rating": [
+            { "name": "Cleanliness", "rating": 4 },
+            { "name": "Comfort", "rating": 4 },
+            { "name": "Spaciousness", "rating": 3 },
+            { "name": "Price", "rating": 2 }
+        ],
+        "mediaSrcs": [],
+        "locationSrc": "",
+        "reviews": [
+            {
+                "reviewerProfile": {
+                    "name": "",
+                    "username": "",
+                    "profileImageSrc": "",
+                    "count": 0
+                },
+                "content": {
+                    "title": "",
+                    "description": "",
+                    "reply": ""
+                },
+                "rating": 0,
+                "score": 0,
+                "mediaSrcs": []
+            },
+        ]
+    },
+
+    "3": {
+        "name": "",
+        "description": "",
+        "address": "",
+        "owner": "",
+        "amenities": ["", ""],
+        "contacts": ["", ""],
+        "rating": [
+            { "name": "Cleanliness", "rating": 4 },
+            { "name": "Comfort", "rating": 4 },
+            { "name": "Spaciousness", "rating": 3 },
+            { "name": "Price", "rating": 2 }
+        ],
+        "mediaSrcs": [],
+        "locationSrc": "",
+        "reviews": [
+            {
+                "reviewerProfile": {
+                    "name": "",
+                    "username": "",
+                    "profileImageSrc": "",
+                    "count": 0
+                },
+                "content": {
+                    "title": "",
+                    "description": "",
+                    "reply": ""
+                },
+                "rating": 0,
+                "score": 0,
+                "mediaSrcs": []
+            },
+        ]
+    },
+}
