e899a78a83b5e7c8642963820297cedfbcc326b4
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-06 11:57:05 +0800

    refactor: updated listing schema to match listing-temp

--- backend/models/Listing.js
+++ backend/models/Listing.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 
 const ListingSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require: true
-    }
+    //Unique identifier
+    listingId: { type: Number, required: true }, 
+
+    //Listing information
+    name: { type: String },
+    address: { type: String },
+    description: { type: String },
+    owner: { type: String },
+    amenities: [String],
+    contacts: [
+        {
+        name: { type: String }, //name of contact (i.e. facebook)
+        link: { type: String }, //link to contact (i.e. facebook link)
+        }
+    ],
+    mediaSrcs: [String],
+    locationSrc: { type: String },
+    rating: [
+        {
+        name: { type: String },
+        rating: { type: Number },
+        }
+    ],
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Listing', ListingSchema);
