29869ed313ee41ddf1af3dee5db7ed5a7d9c2d69
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-17 14:21:00 +0800

    chore: temporarily pruned footer

--- src/components/footer/PageFooter.vue
+++ src/components/footer/PageFooter.vue
@@ -2,10 +2,11 @@
 </script>
 
 <template>
-    <footer class="h-55 w-full flex flex-col justify-center items-center gap-10
+
+    <footer class="h-fit w-full flex flex-col justify-center items-center gap-10 py-20
     bg-[#EAEAEA] dark:bg-[#111111] dark:text-white">
         <!-- Pages -->
-        <div class="w-[90%] flex">
+        <!-- <div class="w-[90%] flex">
             <div class="flex flex-col w-[51%] gap-0.5">
                 <div class="font-bold text-[18px]">Fantastic Dorms</div>
                 <div>About Us</div>
@@ -15,11 +16,11 @@
                 <div class="font-bold text-[18px]">Account</div>
                 <div>Your Account</div>
             </div>
-        </div>
+        </div> -->
 
         <!-- Trademark -->
         <div class="text-[14px] leading-4 text-center text-[#676767]">
-            © 2025 Fantastic Dorms. All rights reserved. • Terms of Use • Privacy Policy
+            © 2025 Fantastic Dorms.  All rights reserved.  •  Terms of Use  •  Privacy Policy
         </div>
     </footer>
 </template>
