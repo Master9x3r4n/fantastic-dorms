1ffc0948eba71cd76a5b8e68481884a51feba18f
nathan <natetrndd@gmail.com>
2026-03-20 03:07:13 +0800

    fix: inter font not applying to the header and footer

--- src/components/footer/PageFooter.vue
+++ src/components/footer/PageFooter.vue
@@ -3,7 +3,7 @@
 
 <template>
     <footer class="h-55 w-full flex flex-col justify-center items-center gap-10
-    bg-[#EAEAEA] dark:bg-[#111111] dark:text-white">
+    bg-[#EAEAEA] dark:bg-[#111111] dark:text-white font-['Inter']">
         <!-- Pages -->
         <div class="w-[90%] flex">
             <div class="flex flex-col w-[51%] gap-0.5">
