3625382871a9c6e3a112fd86f73c8744e949214b
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-19 20:22:26 +0800

    feat: added skeleton template component

---
+++ src/components/skeleton/Skeleton.vue
@@ -0,0 +1,19 @@
+<script setup>
+    
+    const props = defineProps({
+        w: {
+            type: String,
+            default: 'w-full'
+        },
+        h: {
+            type: String,
+            default: 'h-4'
+        }
+    });
+
+</script>
+
+<template>
+    <div class="animate-pulse bg-gray-200 rounded"
+         :class="[w, h]"></div>
+</template>
