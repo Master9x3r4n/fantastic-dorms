bc6b76b9460dd6c45d24ce83ba17b0c5a4c3c60c
nathan <natetrndd@gmail.com>
2026-03-21 05:00:07 +0800

    feat: update Header and Footer Styles

--- src/components/footer/PageFooter.vue
+++ src/components/footer/PageFooter.vue
@@ -2,26 +2,23 @@
 </script>
 
 <template>
-    <footer class="h-55 w-full flex flex-col justify-center items-center gap-10
-    bg-[#EAEAEA] dark:bg-[#111111] dark:text-white font-['Inter']">
-        <!-- Pages -->
-        <div class="w-[90%] flex">
-            <div class="flex flex-col w-[51%] gap-0.5">
-                <div class="font-bold text-[18px]">Fantastic Dorms</div>
-                <div>About Us</div>
-                <div>Contact Page</div>
-            </div>
-            <div class="flex flex-col grow gap-0.5">
-                <div class="font-bold text-[18px]">Account</div>
-                <div>Your Account</div>
-            </div>
-        </div>
+	<footer class="h-55 w-full flex flex-col justify-center items-center gap-10 bg-slate-100 dark:bg-slate-900 text-slate-800 dark:text-slate-100 font-['Inter'] transition-colors duration-200">
+		<!-- Pages -->
+		<div class="w-[90%] flex">
+			<div class="flex flex-col w-[51%] gap-0.5">
+				<div class="font-bold text-[18px]">Fantastic Dorms</div>
+				<div class="cursor-pointer hover:text-slate-600 dark:hover:text-slate-300 transition-colors">About Us</div>
+				<div class="cursor-pointer hover:text-slate-600 dark:hover:text-slate-300 transition-colors">Contact Page</div>
+			</div>
+			<div class="flex flex-col grow gap-0.5">
+				<div class="font-bold text-[18px]">Account</div>
+				<div class="cursor-pointer hover:text-slate-600 dark:hover:text-slate-300 transition-colors">Your Account</div>
+			</div>
+		</div>
 
-        <!-- Trademark -->
-        <div class="text-[14px] leading-4 text-center text-[#676767]">
-            © 2025 Fantastic Dorms. All rights reserved. • Terms of Use • Privacy Policy
-        </div>
-    </footer>
+		<!-- Trademark -->
+		<div class="text-[14px] leading-4 text-center text-slate-500 dark:text-slate-400">
+			© 2025 Fantastic Dorms. All rights reserved. • Terms of Use • Privacy Policy
+		</div>
+	</footer>
 </template>
-
-
