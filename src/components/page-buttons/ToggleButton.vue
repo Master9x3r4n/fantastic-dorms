9259f06291fae227f70d811ee614ec4a94367fed
nathan <natetrndd@gmail.com>
2026-03-21 02:36:43 +0800

    chore: make tailwind classes more readable

--- src/components/page-buttons/ToggleButton.vue
+++ src/components/page-buttons/ToggleButton.vue
@@ -17,7 +17,11 @@ defineEmits(['update:modelValue']);
 				@change="$emit('update:modelValue', $event.target.checked)"
 				class="sr-only peer"
 		/>
-		<div class="w-11 h-6 bg-slate-200 dark:bg-slate-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#355AFF]/30 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border-slate-300 dark:border-slate-600 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#355AFF]"></div>
+		<div class="w-11 h-6 bg-slate-200 dark:bg-slate-700 peer-focus:outline-none peer-focus:ring-4
+								peer-focus:ring-[#355AFF]/30 rounded-full peer peer-checked:after:translate-x-full
+								peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5
+								after:bg-white after:border-slate-300 dark:border-slate-600 after:border after:rounded-full after:h-5
+								after:w-5 after:transition-all peer-checked:bg-[#355AFF]"></div>
 	</label>
 </template>
 
