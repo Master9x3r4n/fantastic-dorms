323dd524e67eefda2b0076ae7f987acdcfb7982c
Jann Miro Quilantang <jannmiroq@gmail.com>
2026-03-23 17:02:42 +0800

    fix: Fix Cloudinary import
    Don't talk to me just talk to Molly Dew

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,8 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
 import express from 'express';
-// import session from 'express-session';
-// import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import { v2 as cloudinary } from 'cloudinary';
 
-// import 'dotenv/config.js';
-// i want to kill whoever designed it this way
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -24,13 +13,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true
-// }));
 app.use(cors());
-app.use(express.json()); // Allows server to read JSON
-cloudinary.config(process.env.CLOUDINARY_URL)
+app.use(express.json());
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+  hide_sensitive: true,
+  private_cdn: false,
+  secure: true
+});
 
 // Database routers
 import AuthRouter from './routers/AuthRouter.js';
@@ -44,25 +36,11 @@ app.use('/api/l/', ListingRouter);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected."))
-    .catch(err => console.log("Connection error:", err));
-
-// Session object
-// app.use(
-//     session({
-//         secret: process.env.SECRET_KEY,
-//         resave: false,
-//         saveUninitialized: false
-//     })
-// );
-// app.use(cookieParser());
+  .then(() => console.log("MongoDB connected."))
+  .catch(err => console.log("Connection error:", err));
 
 app.get('/', (req, res) => {
-    res.send('i want to cry');
+  res.send('I need a Chicago deep dish in my system');
 });
-
-app.get('/api', (req, res) => {
-    res.send('i want to die');
-})
 
 app.listen(PORT, () => console.log(`[Server started on port ${PORT}]`));
