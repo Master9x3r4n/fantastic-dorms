50a71250d4c15d4523618bc99e04b6d20b3fddff
nathan <natetrndd@gmail.com>
2026-03-25 01:15:35 +0800

    fix: remove unresolved access to profileImgSrc

--- src/components/review-cards/ReviewCard.vue
+++ src/components/review-cards/ReviewCard.vue
@@ -51,7 +51,7 @@ const parsedBody = computed(() => {
 					<div class="flex items-center gap-3">
 						<!-- Profile Icon -->
 						<div class="w-12 h-12 rounded-full overflow-hidden bg-slate-200 dark:bg-slate-700 flex items-center justify-center">
-							<ProfileIcon :src="profile.profileImgSrc || profile.picture" sizeClass="w-full h-full" iconSize="text-[24px]!"></ProfileIcon>
+							<ProfileIcon :src="profile.picture" sizeClass="w-full h-full" iconSize="text-[24px]!"></ProfileIcon>
 						</div>
 
 						<!-- Name -->
