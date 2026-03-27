f881fb3c8e5c5bf7fb312bf6a0c734db1cd1dd1c
Jann Miro Quilantang <jannmiroq@gmail.com>
2026-03-27 18:27:10 +0800

    chore: Update main schemas

--- backend/models/Profile.js
+++ backend/models/Profile.js
@@ -1,24 +1,24 @@
 import mongoose from 'mongoose';
 
 const ProfileSchema = new mongoose.Schema({
-    username: { type: String, required: true }, 
-    name: {
-        firstName: { type: String, default: 'Firstname' },
-        lastName: { type: String, default: 'McLastname' }
-    },
-    salt: { type: String },
-    saltedPassword: { type: String },
-    joinDate: { type: Date, default: Date.now },
-    picture: { type: String, default: '' },
-    bio: { type: String, default: 'Goodbye cruel world' },
-    school: {
-        name: { type: String, default: '' },
-        since: { type: String, default: '' }
-    },
-    dorm: {
-        name: { type: String, default: '' },
-        since: { type: String, default: '' }
-    }
+	username: { type: String, required: true }, 
+	name: {
+		firstName: { type: String, default: 'Firstname' },
+		lastName: { type: String, default: 'McLastname' }
+	},
+	salt: { type: String },
+	saltedPassword: { type: String },
+	joinDate: { type: Date, default: Date.now },
+	picture: { type: String, default: '' },
+	bio: { type: String, default: 'Goodbye cruel world' },
+	school: {
+		name: { type: String, default: '' },
+		since: { type: String, default: '' }
+	},
+	dorm: {
+		name: { type: String, default: '' },
+		since: { type: String, default: '' }
+	}
 });
 
 const model = mongoose.model('Profile', ProfileSchema, 'profiles');
