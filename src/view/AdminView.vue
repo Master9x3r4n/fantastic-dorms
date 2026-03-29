0da88701a035d449b2d736fcb982e3864bbb3c30
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-29 22:33:20 +0800

    feat: added table for profiles in admin

--- src/view/AdminView.vue
+++ src/view/AdminView.vue
@@ -4,6 +4,7 @@ import ReviewService from "../services/ReviewService.js";
 import ListingService from "../services/ListingService.js";
 
 import { ref, onMounted } from 'vue';
+import Divider from "@/components/divider/Divider.vue";
 
 //GET USER ADMIN STATUS
 const userInfo = ref('');
@@ -13,7 +14,7 @@ if (data)
 
 // FETCH DATA FROM DB
 const listings = ref([]);
-const profiles = ref(null);
+const profiles = ref([]);
 onMounted(async () => {
     try {
         /****** GET LISTINGS ******/
@@ -57,6 +58,10 @@ function deleteListing (id) {
     console.log("listing id: " + id)
 }
 
+function deleteProfile (id) {
+    console.log("Profile id: " + id)
+}
+
 </script>
 
 <template>
@@ -73,14 +78,16 @@ function deleteListing (id) {
     </div>
     
     <!-- LISTINGS TABLE -->
-    <div class="bg-white rounded-lg shadow-md overflow-hidden overflow-y-scroll">
+    <div class="bg-white rounded-lg shadow-md overflow-hidden overflow-y-scroll max-h-86">
     <table class="min-w-full divide-y divide-gray-200">
         <!-- HEAD -->
         <thead class="bg-gray-100 ">
             <tr><th v-for="i in ['listing_Id', 'name', 'rating', 'owner']"
                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 {{ i }}
-            </th></tr>
+            </th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+            </tr>
         </thead>
         <!-- ROWS -->
         <tbody class="bg-gray-50 divide-y divide-gray-200">
@@ -105,38 +112,39 @@ function deleteListing (id) {
     </div>
 
     <!-- ---------------------------------------------------------------------------------------------- -->
+     <Divider class="my-8"/>
 
     <!-- PROFILES -->
     <div class="flex justify-between items-center mb-8">
-        <h1 class="text-3xl font-bold text-gray-800 dark:text-white">Profiless</h1>
+        <h1 class="text-3xl font-bold text-gray-800 dark:text-white">Profiles</h1>
         <span class="bg-blue-100 text-blue-800 
         px-3 py-1 rounded-full text-sm font-medium">
-        Total Profiles: {{ listings.length }}
+        Total Profiles: {{ profiles.length }}
         </span>
     </div>
     
     <!-- LISTINGS TABLE -->
-    <div class="bg-white rounded-lg shadow-md overflow-hidden overflow-y-scroll">
+    <div class="bg-white rounded-lg shadow-md overflow-hidden overflow-y-scroll max-h-86">
     <table class="min-w-full divide-y divide-gray-200">
         <!-- HEAD -->
         <thead class="bg-gray-100 ">
-            <tr><th v-for="i in ['listing_Id', 'name', 'rating', 'owner']"
+            <tr><th v-for="i in ['username', 'name', 'join date', 'listing owner']"
                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 {{ i }}
-            </th></tr>
+            </th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+            </tr>
         </thead>
         <!-- ROWS -->
         <tbody class="bg-gray-50 divide-y divide-gray-200">
-            <tr v-for="l in listings" :key="l._id" class="hover:bg-gray-50 transition">
-                <td class="px-6 py-4 whitespace-nowrap font-medium text-gray-900">{{ l.listingId }}</td>
-                <td class="px-6 py-4 whitespace-nowrap text-gray-600">{{ l.name }}</td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                    {{ parseRating(l.rating) }}/5
-                </td>
-                <td class="px-6 py-4 text-gray-500 truncate max-w-xs">{{ l.owner }}</td>
+            <tr v-for="p in profiles" :key="p._id" class="hover:bg-gray-50 transition">
+                <td class="px-6 py-4 whitespace-nowrap font-medium text-gray-900">{{ p.username }}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-gray-600">{{ p.name.lastName }}, {{ p.name.firstName }}</td>
+                <td class="px-6 py-4 text-gray-500 truncate max-w-xs">{{ p.joinDate }}</td>
+                <td class="px-6 py-4 text-gray-500 truncate max-w-xs"> false </td>
                 <td class="px-6 py-4 whitespace-nowrap text-right">
                     <button 
-                    @click="deleteListing(l._id)"
+                    @click="deleteProfile(p._id)"
                     class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm font-semibold transition"
                     >
                     Delete
