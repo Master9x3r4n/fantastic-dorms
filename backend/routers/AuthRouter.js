8c8a5fd3924b2fcfb86e1dcef32352cbbe768169
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-02 19:16:06 +0800

    fix: profile not properly stores in session

--- backend/routers/AuthRouter.js
+++ backend/routers/AuthRouter.js
@@ -4,13 +4,27 @@
 
 import express from 'express';
 import ProfileController from '../controllers/ProfileController.js';
+import Profile from '../models/Profile.js';
 const router = express.Router();
 
 router.post('/login', ProfileController.login);
 
-router.get('/me', (req, res) => {
-  if (req.session.user) res.status(200).json({ user: req.session.user });
-  else res.status(401).json({ message: 'Not logged in.' });
+router.get('/me', async (req, res) => {
+  //if (req.session.user) res.status(200).json({ user: req.session.user });
+  if (!req.session.user)
+  {
+    return res.status(401).json({ message: 'Not logged in.' });
+  }
+  try 
+  {
+    const profile = await Profile.findOne({ username: req.session.user.username });
+    if (profile) res.status(200).json({ user: profile });
+    else res.status(404).json({ message: 'Profile not found.' });
+  } catch (err)
+  {
+    res.status(500).json({ message: err.message + ' for ' + JSON.stringify(req.session.user)});
+  }
+
 });
 
 router.post('/logout', (req, res) => {
