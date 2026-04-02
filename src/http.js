c1167c767750f680f17307cfcbfdc6076a49b31f
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-02 17:47:17 +0800

    miro: wow

--- src/http.js
+++ src/http.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 const port = import.meta.env.VITE_PORT;
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true; //bruh i cant believe miro was hiding this line of code from me - stephy
 const api = axios.create({
     baseURL: `http://localhost:${port}/api`,
     headers: {
         "Content-type": "application/json"
-    },
-    withCredentials: true
+    }
 });
 
 export default api;
