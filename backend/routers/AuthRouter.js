d26a720043b0a648dc13e73929fff48d7e5580d8
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-02 17:35:12 +0800

    feat: added router authentication if user is logged in and user has logged out

--- backend/routers/AuthRouter.js
+++ backend/routers/AuthRouter.js
@@ -8,4 +8,17 @@ const router = express.Router();
 
 router.post('/login', ProfileController.login);
 
+router.get('/me', (req, res) => {
+  if (req.session.user) res.status(200).json({ user: req.session.user });
+  else res.status(401).json({ message: 'Not logged in.' });
+});
+
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) return res.status(500).json({ message: 'Logout failed.' });
+    res.clearCookie('connect.sid');
+    res.status(200).json({ message: 'Logged out.' });
+  });
+});
+
 export default router;
