78b7d87c718208d9c703ba62efb7cd3a9a3d1f36
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-02 17:38:06 +0800

    feat: connected session authentication for log in to profile router

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,4 @@
+// used to authorize session handling
 const requireAuth = (req, res, next) => {
   if (req.session?.user) next();
   else res.status(401).json({ message: 'Unauthorized. Please log in.' });
