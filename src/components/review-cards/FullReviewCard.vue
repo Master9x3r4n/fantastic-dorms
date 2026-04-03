d879a9de7b9c5e14ab6725eafdf5e3caf4d0ad03
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-04 01:42:47 +0800

    style: Reply review now appears above thumbs buttons

--- src/components/review-cards/FullReviewCard.vue
+++ src/components/review-cards/FullReviewCard.vue
@@ -125,6 +125,11 @@ const parsedBody = computed(() => {
 				:truncate="false"
 		/>
 
+		<ReplyReview 
+		v-if="showReview"
+		@closeReview="showReview = false"
+		/>
+
 		<div class="flex w-full justify-between items-center">
 			<!-- Thumbs :D -->
 			<div class="flex items-center gap-3.5 text-slate-500 dark:text-slate-400 pt-1">
@@ -140,11 +145,6 @@ const parsedBody = computed(() => {
 				<span v-else>Edit Reply</span>
 			</BlueButton>
 		</div>
-
-		<ReplyReview 
-		v-if="showReview"
-		@closeReview="showReview = false"
-		/>
 
 	</div>
 </template>
