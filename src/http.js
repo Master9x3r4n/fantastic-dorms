c1f665c46329adac995165de6e908da41155190d
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-03 16:59:33 +0800

    cycles: 33550337 cycles of deployment issues

--- src/http.js
+++ src/http.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-const port = import.meta.env.VITE_PORT;
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 axios.defaults.withCredentials = true; 
 
 const api = axios.create({
-    baseURL: `http://localhost:${port}/api`,
+    baseURL: `${import.meta.env.VITE_API_URL || 'http://localhost:3000'}/api`,
     headers: {
         "Content-type": "application/json"
     }
