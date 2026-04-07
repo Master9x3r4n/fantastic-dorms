6a865cf84fde877e4a4b26be61cc52201fee0b3d
Stephen <stephen.jszs.gplay@gmail.com>
2026-04-07 13:04:44 +0800

    fix: i routed the wrong thing

--- src/view/RegisterView.vue
+++ src/view/RegisterView.vue
@@ -67,7 +67,7 @@
 		try {
 			await ProfileService.create(data);
 			await ProfileService.login({ username: data.username, password: data.password });
-			await router.push('/');
+			await router.push('/settings');
 		} catch (err) {
 			const message = err.response?.data?.message 
 				|| err.response?.data?.errors?.[0]?.msg
@@ -218,7 +218,7 @@
 					<!-- Login Link -->
 					<p class="text-center text-slate-600 dark:text-slate-400 text-sm transition-colors">
 						Already have an account?
-						<RouterLink to="/settings">
+						<RouterLink to="/login">
 							<a class="text-[#355AFF] hover:underline font-semibold" href="#">Login</a>
 						</RouterLink>
 					</p>
